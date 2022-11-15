f45a36046de3e6361d569b7a02f41a4b102fda48
David León Gómez <dleon55@hotmail.com>
2022-11-14 21:21:47 -0600

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Dragon Ball"]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory.trim())) return;
+    // Agrega caterogias al arreglo
+    setCategories([newCategory, ...categories]);
+    // setCategories((cats) => [...cats, 'Hola Mundo']);
+  };
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <hr />
+      <AddCategory onNewCategory={(value) => onAddCategory(value)} />
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
